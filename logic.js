1a326b17ecd2f74c1989072f2034935979f4f9cb
Ryan Gabbard <gabbard@isi.edu>
2020-01-15 00:50:41 -0500

    Explicit dates; Manual sort trigger for dynamic task loading
    * Task dates of the form "Dec 15" or "Jan 31, 2021" are now supported.
    * Asana seems to load tasks dynamically, meaning not all tasks will be
    available to sort on page load.  The user can now manually trigger the
    sort via Ctrl-D.

--- logic.js
+++ logic.js
@@ -3,15 +3,28 @@
 // The function to do the sorting of each group comes first,
 // followed by the logic to find each task group and apply the function to it.
 
+function sortAllTaskGroups() {
+    // Logic for finding and task groups and applying the sorting function to each one.
+    // this is the div which contains the Recently Assigned, Today, and Upcoming task groups
+    const taskGroupContainers = document.getElementsByClassName("TaskGroup-subgroups");
+
+    for (let taskGroupContainer of taskGroupContainers) {
+        for (let taskGroupDiv of taskGroupContainer.children) {
+                sortTasksByDueDate(taskGroupDiv);
+        }
+    }
+}
+
 // Make usage of the JSJoda date library more compact below.
 const LocalDate = JSJoda.LocalDate;
 const DayOfWeek = JSJoda.DayOfWeek;
 const TemporalAdjusters = JSJoda.TemporalAdjusters;
+const DateTimeFormatter = JSJoda.DateTimeFormatter;
+const Month = JSJoda.Month;
 
 const knownTaskGroupNames = ["Recently assigned", "Today", "Upcoming"];
 
 function sortTasksByDueDate(taskGroupDiv) {
-    console.log(taskGroupDiv);
     const taskGroupHeaderDiv = taskGroupDiv.children[0];
     if (knownTaskGroupNames.includes(taskGroupHeaderDiv.textContent)) {
         console.log("Sorting " + taskGroupHeaderDiv.textContent);
@@ -66,6 +79,7 @@ function extractTaskDate(task) {
         const dateString = dateElement.textContent;
 
         const dayOfWeek = toDayOfWeek(dateString);
+        const explicitDate = attemptToParseDate(dateString);
 
         if (dateString == "Today") {
             return currentDate;
@@ -76,11 +90,12 @@ function extractTaskDate(task) {
         } else if (dayOfWeek != null) {
             // e.g. "Wednesday"
             return currentDate.with(TemporalAdjusters.next(dayOfWeek));
+        } else if (explicitDate != null) {
+            // Dec 15
+            // Jan 5, 2021
+            return explicitDate;
         } else {
-            // TODO: Dec 15
-            // TODO: Jan 5, 2021
-            console.log("Don't know how to parse " + dateString);
-            return null;
+            console.log("Don't know how to parse date string " + dateString);
         }
     } else {
         // No due date is specified for this task. 
@@ -109,13 +124,47 @@ function toDayOfWeek(dateString) {
     }
 }
 
-// Logic for finding and task groups and applying the sorting function to each one.
-// this is the div which contains the Recently Assigned, Today, and Upcoming task groups
-const taskGroupContainers = document.getElementsByClassName("TaskGroup-subgroups");
-
-for (let taskGroupContainer of taskGroupContainers) {
-   console.log(taskGroupContainer);
-   for (let taskGroupDiv of taskGroupContainer.children) {
-        sortTasksByDueDate(taskGroupDiv);
-    }
+// JS-Joda has date parsing support,
+// but it required additional sub-libraries to work with text strings
+// and I was having trouble getting it to work,
+// so I rolled my own for now.
+const namesToMonths = {
+    'Jan' : Month.JANUARY,
+    'Feb' : Month.FEBRUARY,
+    'Mar' : Month.MARCH,
+    'Apr' : Month.APRIL,
+    'May' : Month.MAY,
+    'Jun' : Month.JUNE,
+    'Jul' : Month.JULY,
+    'Aug' : Month.AUGUST,
+    'Sep' : Month.SEPTEMBER,
+    'Oct' : Month.OCTOBER,
+    'Nov' : Month.NOVEMBER,
+    'Dec' : Month.DECEMBER
 }
+
+function attemptToParseDate(dateString) {
+    console.log("Attempting to parse " + dateString);
+    const dateParts = dateString.split(/[\s,]/);
+    console.log("parts " + dateParts + "/" + dateParts.length);
+
+    if (dateParts.length > 1) {
+        const maybeMonth = dateParts[0];
+        if (maybeMonth in namesToMonths) {
+            console.log("Recognized month");
+            var day = dateParts[1];
+            var year = LocalDate.now().year();
+            if (dateParts.length == 4) {
+                year = dateParts[3];
+            }
+            console.log("Year = " + year + " day = " + day + " month=" + maybeMonth);
+            return new LocalDate(parseInt(year), namesToMonths[maybeMonth], parseInt(day))
+        }
+    } 
+    // not a parseable date
+    console.log("Couldn't parse it");
+    return null;
+}
+
+sortAllTaskGroups()
+key('ctrl+d', function(){ alert('you pressed a!'); sortAllTaskGroups(); alert("Sorted all task groups"); });
